ef9da4e0ff5ce53c7036f5739c69808ed35dd814
Olep8h <panov@student.vspj.cz>
2022-11-17 02:45:27 +0200

    implementation jsx for rendering. work with HangmanDrawing.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import {useState} from "react";
+import { useCallback, useEffect, useState } from "react"
+import { HangmanDrawing } from "./HangmanDrawing"
+import { HangmanWord } from "./HangmanWord"
+import { Keyboard } from "./Keyboard"
 import words from "./wordList.json"
-
 function App() {
-    const [wordToGuess,setWordToGuess] = useState(() => {
+    const [wordToGuess, setWordToGuess] = useState(() => {
         return words[Math.floor(Math.random() * words.length)]
     })
     const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
- console.log(wordToGuess)
-    return <h1>Hi</h1>
-}
-
-export default App
+    return (
+        <div
+            style={{
+                maxWidth: "800px",
+                display: "flex",
+                flexDirection: "column",
+                gap: "2rem",
+                margin: "0 auto",
+                alignItems: "center",
+            }}
+        >
+            <div style={{ fontSize: "2rem", textAlign: "center" }}>
+                {isWinner && "Winner! - Refresh to try again"}
+                {isLoser && "Nice Try - Refresh to try again"}
+            </div>
+            export default App
