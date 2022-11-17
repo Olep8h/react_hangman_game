422bcff2ee1348d2d8dc1bdd5d76569c14ed9dae
Olep8h <panov@student.vspj.cz>
2022-11-17 04:30:53 +0200

    some component logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,65 @@ import { HangmanDrawing } from "./HangmanDrawing"
 import { HangmanWord } from "./HangmanWord"
 import { Keyboard } from "./Keyboard"
 import words from "./wordList.json"
+
+function getWord() {
+    return words[Math.floor(Math.random() * words.length)]
+}
+
 function App() {
-    const [wordToGuess, setWordToGuess] = useState(() => {
-        return words[Math.floor(Math.random() * words.length)]
-    })
+    const [wordToGuess, setWordToGuess] = useState(getWord)
     const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+    const incorrectLetters = guessedLetters.filter(
+        letter => !wordToGuess.includes(letter)
+    )
+
+    const isLoser = incorrectLetters.length >= 6
+    const isWinner = wordToGuess
+        .split("")
+        .every(letter => guessedLetters.includes(letter))
+
+    const addGuessedLetter = useCallback(
+        (letter: string) => {
+            if (guessedLetters.includes(letter) || isLoser || isWinner) return
+
+            setGuessedLetters(currentLetters => [...currentLetters, letter])
+        },
+        [guessedLetters, isWinner, isLoser]
+    )
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            const key = e.key
+            if (!key.match(/^[a-z]$/)) return
+
+            e.preventDefault()
+            addGuessedLetter(key)
+        }
+
+        document.addEventListener("keypress", handler)
+
+        return () => {
+            document.removeEventListener("keypress", handler)
+        }
+    }, [guessedLetters])
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            const key = e.key
+            if (key !== "Enter") return
+
+            e.preventDefault()
+            setGuessedLetters([])
+            setWordToGuess(getWord())
+        }
+
+        document.addEventListener("keypress", handler)
+
+        return () => {
+            document.removeEventListener("keypress", handler)
+        }
+    }, [])
 
     return (
         <div
@@ -24,4 +78,13 @@ function App() {
                 {isWinner && "Winner! - Refresh to try again"}
                 {isLoser && "Nice Try - Refresh to try again"}
             </div>
-            export default App
+            <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+            <HangmanWord/>
+            <div style={{ alignSelf: "stretch" }}>
+                <Keyboard />
+            </div>
+        </div>
+    )
+}
+
+export default App
--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -85,42 +85,43 @@ const LEFT_LEG = (
     />
 )
 
-export function HangmanDrawing() {
-return (
-    <div style={{ position: "relative" }}>
-        {HEAD}
-        {BODY}
-        {LEFT_ARM}
-        {RIGHT_ARM}
-        {LEFT_LEG}
-        {RIGHT_LEG}
-        <div
-            style={{
-                height: "50x",
-                width: "10px",
-                background: "black",
-                top: 0,
-                right: 0,
-            }}
-        />
-        <div
-            style={{
-                height: "10x",
-                width: "200px",
-                background: "black",
-                marginLeft: "120px",
-            }}
-        />
-    <div
-        style={{
-        height: "400px",
-        width: "10px",
-        background: "black",
-        marginLeft: "120px",
-    }}
-/>
-    <div style={{ height: "10px", width: "250px", background:
-            "black" }} />
-</div>
-)
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG]
+
+type HangmanDrawingProps = {
+    numberOfGuesses: number
+}
+
+export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
+    return (
+        <div style={{ position: "relative" }}>
+            {BODY_PARTS.slice(0, numberOfGuesses)}
+            <div
+                style={{
+                    height: "50px",
+                    width: "10px",
+                    background: "black",
+                    position: "absolute",
+                    top: 0,
+                    right: 0,
+                }}
+            />
+            <div
+                style={{
+                    height: "10px",
+                    width: "200px",
+                    background: "black",
+                    marginLeft: "120px",
+                }}
+            />
+            <div
+                style={{
+                    height: "400px",
+                    width: "10px",
+                    background: "black",
+                    marginLeft: "120px",
+                }}
+            />
+            <div style={{ height: "10px", width: "250px", background: "black" }} />
+        </div>
+    )
 }
