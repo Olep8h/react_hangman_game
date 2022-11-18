28c7a00ebe1a96a96b89e6ff75a9f01b7d850a37
Olep8h <panov@student.vspj.cz>
2022-11-18 15:30:47 +0200

    import files to index.js and fix problem

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import "./Keyboard.module.css"
 
 const KEYS = [
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import React from "react"
 import "./index.css"
+import "./App"
+import "./Keyboard.module.css"
+import "./main"
+import "./HangmanDrawing"
+import "./Keyboard"
+import "./HangmanWord"
+
